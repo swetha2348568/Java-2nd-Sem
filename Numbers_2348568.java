6b36713b2504208a9061f0aa6ebc8e453c9340ec
swetha2348568 <139948050+swetha2348568@users.noreply.github.com>
2023-12-16 12:01:03 +0530

    Add files via upload

---
+++ Numbers_2348568.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+    public class Numbers_2348568 {
+     static int[][] arr;
+     static int K;
+     
+     public public static void main(String args[]) {
+        
+     
+         Scanner obj=new Scanner();
+         System.out.println("Enter the size of the array: ");
+         N=obj.nextInt();
+         System.out.println("Enter the frequency: ");
+         K=obj.nextInt();
+         System.out.println("Enter the values: ");
+         for (int i=0; i<N; i++){
+             System.out.println("Value: " +(i + 1) + ": ");
+             arr[i]=obj.nextInt();
+        }
+    }
+       }
+    
+     
+     
+
